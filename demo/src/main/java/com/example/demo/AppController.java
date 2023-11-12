400ef41d478831547fc6bc1ef05e007fe33ed1b9
Ahilan0499 <ahilanmahesh@gmail.com>
2023-11-12 18:53:16 +0530

    sso

---
+++ demo/src/main/java/com/example/demo/AppController.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+import java.security.Principal;
+import java.util.Map;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AppController {
+
+    @GetMapping("/")
+    public String Home() {
+        return "http://localhost:8180/realms/sso/protocol/openid-connect/auth?response_type=code&client_id=sso_client&scope=openid&state=Iddr65QgMMtxLXafD2BdqTJhuLfs5695ObxSDonDoL0%3D&redirect_uri=http://localhost:8080/login/oauth2/code/keycloak&nonce=py0r-UBMRhAcaYXJBVxfK8g0UP6U7kCCzzHg9De8x9A";
+    }
+
+    @GetMapping("/cloudinsider")
+    public String Cloud(){
+        return "cloudinsider";
+    }
+
+    // @GetMapping("/hello")
+    // public String Cloud(){
+    //     return ("hello");
+    // }
+
+
+    @GetMapping("/protected")
+    @PreAuthorize("hasRole(ROLE_USER)")
+    public String Protected(Principal principal, Model model) {
+        model.addAttribute("username", principal.getName());
+        return "protected";
+    }
+
+    @GetMapping("/dashboard")
+    public String Test(Principal principal, Model model) {
+        final DefaultOidcUser user = (DefaultOidcUser) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+        OidcIdToken token = user.getIdToken();
+        Map<String, Object> customClaims = token.getClaims();
+        model.addAttribute("userdetails", customClaims.get("name"));
+        return "dashboard";
+    }
+}
---
+++ demo/src/main/webapp/WEB-INF/views/cloudinsider.jsp
@@ -0,0 +1,21 @@
+%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<meta charset="ISO-8859-1">
+  <body>
+    <h1>Global Solution Applications</h1>
+    <body class="">
+        <div class="">
+    
+        <h2>hi</h2>
+    
+        
+    </body>
+    </html>
+ </body>
+</html>
